0fe8bb8432eddb70ef4e1faa625f6e62ec5e9917
playmaker26 <jorge.artaza22@gmail.com>
2024-07-30 06:35:11 -0400

    debugging active link

--- js/script.js
+++ js/script.js
@@ -20,14 +20,17 @@ toggleNav();
 //active link 
 
 let activeLink = function () {
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('.nav-links')
+const activePage = window.location.pathname.split('/').pop();
+console.log('Active page:', activePage);
+const navLinks = document.querySelectorAll('.nav-links');
 
 navLinks.forEach(link => {
-    const linkPath = new URL(link.href).pathname;
+   const linkPath = link.getAttribute('href').split('/').pop();
+   console.log('Link path:', linkPath);
 
     if(linkPath === activePage) {
         link.classList.add('active');
+        console.log('Active Link:', link.href);
     }
 });
 }
