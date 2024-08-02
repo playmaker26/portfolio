587d584b753675dc556f00d8644ba47e8e251cac
playmaker26 <jorge.artaza22@gmail.com>
2024-08-02 19:41:48 -0400

    almost done with modal

--- js/script.js
+++ js/script.js
@@ -62,12 +62,122 @@ let intersectionObserver = function () {
 
 intersectionObserver();
 
+//modal
+const projects = {
+    traveler: {
+        image: '../project-imge/traveler.jpeg',
+        name: 'Traveler',
+        description: 'Traveler is a beautifully designed website that serves as an inspiration hub for travel enthusiasts. It showcases breathtaking destinations, travel tips, and engaging stories to help you plan your next adventure. The site is built with a focus on visual appeal and user experience, making it easy to navigate and explore various travel ideas.',
+        liveDemo: 'https://playmaker26.github.io/Traveler/',
+        github: 'https://github.com/playmaker26/Traveler',
+        figma: 'https://www.figma.com/design/0sZRy2wPSbDlHaSbgXMEXQ/Portfolio?node-id=0-1&t=t2TKaTmWS75ZDR0c-1'
+    },
+    memoryGame: {
+        image: '../project-imge/memory-game.png',
+        name: 'Memory Game',
+        description: 'The Super Mario Memory Game is a fun and interactive web application designed to test and improve your memory skills. Inspired by the beloved Super Mario series, this game features iconic characters and items from the franchise. Players are challenged to find matching pairs of cards within a grid, with each successful match revealing a piece of the hidden image beneath.',
+        liveDemo: 'https://playmaker26.github.io/Memory-Game/',
+        github: 'https://github.com/playmaker26/Memory-Game',
+        figma: 'https://www.figma.com/design/0PRMZRgRLvPIUUGS8bjglg/Super-Mario-Memory-Game?node-id=0-1&t=JoWzykj9TPSaOqZw-1'
+    }
+};
 
-let modal = function () {
-let modalLinks = document.querySelectorAll('.link-modal');
+let modalFunction = function () {
+    let modalLinks = document.querySelectorAll('.link-modal');
 
-let modal =`<dialog id="my_dialog">
+    modalLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
 
-</dialog>`
-}
-modal();
+            // Determine which project details to use
+            let projectKey = link.parentNode.querySelector('p').textContent.toLowerCase().replace(' ', '');
+            let project = projects[projectKey];
+
+            // Create modal elements
+            let modal = document.createElement('dialog');
+
+            modal.innerHTML = `
+                <div>
+                    <button id="close_modal" class="close">Close</button>
+                    <figure>
+                        <img src="${project.image}" alt="${project.name}">
+                        <figcaption>
+                            <header>
+                                <h1>${project.name}</h1>
+                            </header>
+                            <p>${project.description}</p>
+                            <article>
+                                <header>
+                                    <h2>Where to find ${project.name}</h2>
+                                </header>
+                                <figure>
+                                    <i class="fa-solid fa-desktop"></i>
+                                    <figcaption>
+                                        <a href="${project.liveDemo}" target="_blank">Live Demo</a>
+                                    </figcaption>
+                                </figure>
+                                <figure>
+                                    <i class="fa-brands fa-github"></i>
+                                    <figcaption>
+                                        <a href="${project.github}" target="_blank">GitHub</a>
+                                    </figcaption>
+                                </figure>
+                                <figure>
+                                    <i class="fa-brands fa-figma"></i>
+                                    <figcaption>
+                                        <a href="${project.figma}" target="_blank">Figma</a>
+                                    </figcaption>
+                                </figure>
+                            </article>
+                            <article>
+                                <header>
+                                    <h3>Technologies used</h3>
+                                </header>
+                                <figure>
+                                    <i class="fa-brands fa-html5"></i>
+                                    <figcaption>
+                                        <p>HTML</p>
+                                    </figcaption>
+                                </figure>
+                                <figure>
+                                    <i class="fa-brands fa-css3-alt"></i>
+                                    <figcaption>
+                                        <p>CSS</p>
+                                    </figcaption>
+                                </figure>
+                                <figure>
+                                    <i class="fa-brands fa-js"></i>
+                                    <figcaption>
+                                        <p>JavaScript</p>
+                                    </figcaption>
+                                </figure>
+                            </article>
+                        </figcaption>
+                    </figure>
+                </div>
+            `;
+
+            // Append modal to body
+            document.body.appendChild(modal);
+
+            // Show modal
+            modal.showModal();
+
+            // Close modal when the close button is clicked
+            document.getElementById('close_modal').addEventListener('click', function() {
+                modal.close();
+                document.body.removeChild(modal);
+            });
+
+            // Close modal when clicking outside of it
+            modal.addEventListener('click', function(event) {
+                if (event.target === modal) {
+                    modal.close();
+                    document.body.removeChild(modal);
+                }
+            });
+        });
+    });
+};
+
+modalFunction();
